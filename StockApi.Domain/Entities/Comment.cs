da26aa4fd77c78a18975d55aa3ca83fd81f6d65f
kietnv1974 <kiet.nv@isdcorp.vn>
2025-10-07 23:11:32 +0700

    update

--- StockApi.Domain/Entities/Comment.cs
+++ StockApi.Domain/Entities/Comment.cs
@@ -9,9 +9,13 @@ namespace StockApi.Domain.Entities
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty; 
         public DateTime CreateOn { get; set; } = DateTime.Now;
+
         public int? StockId { get; set; }
         public Stock? Stock { get; set; }
+
+        public int? UserId { get; set; }
+        public User? User { get; set; }
     }
 }
---
+++ StockApi.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockApi.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty; 
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Navigation property: Một user có thể có nhiều comment
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
